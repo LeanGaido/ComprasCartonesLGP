1e2c132d0ade0c2727ad0e08b6cb5d2eba2e1d19
javier sicardi <javisicardi94@gmail.com>
2021-09-27 10:38:01 -0300

    Contentadmin en proceso

---
+++ ComprasCartonesLGP.Web/Areas/ContentAdmin/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComprasCartonesLGP.Web.Areas.ContentAdmin.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize(Roles = "Admin")]
+        // GET: ContentAdmin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
